1648c8f71b8d3b0e3c86e8b3d75b251f94cd7d1e
PanchoGamer <jorgesaa36@gmail.com>
2026-09-15 19:08:19 -0300

    Creacion Repositorio y Menu Basico

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int main() {
+    cout << "------------------- Menu Principal -------------------" << endl;
+    int opcion;
+    do{
+        cout << "1. Cargar Pacientes." << endl;
+        cout << "2. Mostrar cola de pacientes pendientes" << endl;
+        cout << "3. Atender a cierta cantidad" << endl;
+        cout << "4. Mostrar el estado general de los servicios" << endl;
+        cout << "5. Mostrar el historial de atenciones" << endl;
+        cout << "6. Salir" << endl;
+        cout << "Eliga la opcion: ";
+        cin >> opcion;
+        cout << "" << endl;
+
+        if (cin.fail()){
+            throw invalid_argument("Error. Use solo numeros");
+        }
+
+        if (opcion < 1 || opcion > 6){
+            throw invalid_argument("Eliga una opcion valida");
+        }
+
+        switch(opcion){
+            case 1:
+                cout << "Opcion1" << endl;
+            case 2:
+                cout << "Opcion2" << endl;
+        };
+        
+
+    } while (opcion != 6);
+}
